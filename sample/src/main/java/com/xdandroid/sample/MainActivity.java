a9a06c63341191a4e50969f9fd7f2b48a931ef17
Da Xing <d.x@fowtech.com>
2017-03-08 14:45:06 +0800

    Unuse of Jack.

--- sample/src/main/java/com/xdandroid/sample/MainActivity.java
+++ sample/src/main/java/com/xdandroid/sample/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.*;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.*;
 
 import com.xdandroid.materialprogressview.*;
 
@@ -15,15 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
-        MaterialProgressView progressView = (MaterialProgressView) findViewById(R.id.progress_view);
+        final MaterialProgressView progressView = (MaterialProgressView) findViewById(R.id.progress_view);
 
-        findViewById(R.id.button0).setOnClickListener(v -> {
-            progressView.setColorSchemeColors(new int[]{getResources().getColor(R.color.colorPrimary), getResources().getColor(R.color.colorAccent)});
-            progressView.setProgressBackgroundColor(Color.parseColor("#FAFAFA"));
+        findViewById(R.id.button0).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                progressView.setColorSchemeColors(new int[]{MainActivity.this.getResources().getColor(R.color.colorPrimary), MainActivity.this.getResources().getColor(R.color.colorAccent)});
+                progressView.setProgressBackgroundColor(Color.parseColor("#FAFAFA"));
+            }
         });
-        findViewById(R.id.button1).setOnClickListener(v -> {
-            progressView.setColorSchemeColors(new int[]{getResources().getColor(android.R.color.darker_gray)});
-            progressView.setProgressBackgroundColor(Color.parseColor("#333333"));
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                progressView.setColorSchemeColors(new int[]{MainActivity.this.getResources().getColor(android.R.color.darker_gray)});
+                progressView.setProgressBackgroundColor(Color.parseColor("#333333"));
+            }
         });
     }
 }
